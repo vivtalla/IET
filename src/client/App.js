769d938cbbacdfa3cbe0dab2afec6ef79a70d2ed
Kevin Ray <emilyray@Kevins-MacBook-Air.local>
2019-02-01 19:54:27 -0600

    Added node server basics

--- src/client/App.js
+++ src/client/App.js
@@ -8,14 +8,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarOpen: true
+      sidebarOpen: true,
+      response: "",
+      post: "",
+      responseToPost: ""
     };
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    this.onCallApi = this.onCallApi.bind(this);
   }
 
   onSetSidebarOpen(open) {
     this.setState({sidebarOpen: open});
   }
+
+  onCallApi() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+
+
+  callApi = async () => {
+    const response = await fetch('/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
 
   render() {
     return (
@@ -29,6 +48,10 @@ class App extends Component {
         <button onClick={() => this.onSetSidebarOpen(true)}>
           Open Sidebar
         </button>
+        <button onClick={() => this.onCallApi()}>
+          Call Api
+        </button>
+        <b style={{color:"white"}}>{this.state.response}</b>
       </Sidebar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
