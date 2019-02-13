6592326a87dc722d9f9076add10a2ce60ea63b31
Johnathon Bliss <31452265+jbliss9821@users.noreply.github.com>
2019-02-12 18:22:07 -0600

    Popup goal choices
    Updated the popup so that choosing a goal component places in different filler text as based on choice and closes the sub menu.  Issue with clicking document not updating popup default message for now.

--- src/client/FormCreationMenu.js
+++ src/client/FormCreationMenu.js
@@ -6,6 +6,16 @@ import ListItem from '@material-ui/core/ListItem';
 export class FormCreationMenu extends Component {
     constructor(props) {
         super(props);//only leaving this here in case this.props is needed in the constructor
+        this.state = {
+            popupContent: "chooseMenu"
+          };
+        this.onListItemClicked = this.onListItemClicked.bind(this);
+    }
+
+    onListItemClicked(popupContent){
+        this.setState({
+          popupContent: popupContent
+        })
     }
 
     render() {
@@ -13,47 +23,53 @@ export class FormCreationMenu extends Component {
             <Popup trigger={<button className="button"> Add Goal Component </button>} modal lockScroll = {true}>
             {close => (
                 <div className="modal">
-                    <a className="close" onClick={close}>
+                    {/* <a className="close" onClick={()=>this.onListItemClicked("chooseMenu")}>
                     &times;
-                    </a>
-                    <div className="header">Add Goal </div>
-                    <div className="content">
-                    {' '}
-                    Choose New Goal Component Type From List
-                    </div>
+                    </a> */}
+                    <div className="header">Add Goal Component </div>
+                    {this.state.popupContent == "chooseMenu" ? <ChooseMenu /> : 
+                    this.state.popupContent == "yesNoGoal"? <YesNoGoal /> : 
+                    this.state.popupContent == "timerGoal"? <TimerGoal /> :
+                    this.state.popupContent == "incrementGoal"? <IncrementGoal /> :
+                    this.state.popupContent == "textboxGoal"? <TextboxGoal /> :
+                    this.state.popupContent == "dropdownGoal"? <DropdownGoal /> : null}
                     <div className="actions">
-                    <Popup
-                        trigger={<button className="button"> Component Types </button>}
-                        position="top center"
-                        closeOnDocumentClick
-                    >
-                        <List>
+                        <Popup
+                            trigger={<button className="button"> Component Types </button>}
+                            position="top center"
+                            closeOnDocumentClick
+                        >
+                        {close1 => (
+                            <List>
                             {/* <div className="menu"> */}
-                            <ListItem button className="menu-item" title="menu_yesno"> Yes/No</ListItem>
-                            <ListItem button className="menu-item" title="menu_timer"> Timer</ListItem>
-                            <ListItem button className="menu-item" title="menu_increment"> Increment</ListItem>
-                            <ListItem button className="menu-item" title="menu_textbox"> TextBox</ListItem>
-                            <ListItem button className="menu-item" title="menu_dropdown"> DropDown</ListItem>
+                            <ListItem button className="menu-item" title="menu_yesno" onClick={()=>{this.onListItemClicked("yesNoGoal"); close1()}}> Yes/No</ListItem>
+                            <ListItem button className="menu-item" title="menu_timer" onClick={()=>{this.onListItemClicked("timerGoal"); close1()}}> Timer</ListItem>
+                            <ListItem button className="menu-item" title="menu_increment" onClick={()=>{this.onListItemClicked("incrementGoal"); close1()}}> Increment</ListItem>
+                            <ListItem button className="menu-item" title="menu_textbox" onClick={()=>{this.onListItemClicked("textboxGoal"); close1()}}> TextBox</ListItem>
+                            <ListItem button className="menu-item" title="menu_dropdown" onClick={()=>{this.onListItemClicked("dropdownGoal"); close1()}}> DropDown</ListItem>
                             {/* </div> */}
                         </List>
-                    </Popup>
-                    <button 
-                        className="button"
-                        onClick={()=> {
-                            close()
-                        }}
-                    >
-                        Apply
-                    </button>
+                        )}
+                        </Popup>
+                        <button 
+                            className="button"
+                            onClick={()=> {
+                                close();
+                                this.onListItemClicked("chooseMenu");
+                            }}
+                        >
+                            Apply
+                        </button>
 
-                    <button
-                        className="button"
-                        onClick={() => {
-                        close()
-                        }}
-                    >
-                        Cancel
-                    </button>
+                        <button
+                            className="button"
+                            onClick={() => {
+                            close();
+                            this.onListItemClicked("chooseMenu");
+                            }}
+                        >
+                            Cancel
+                        </button>
                     </div>
                 </div>
                 )}
@@ -63,56 +79,95 @@ export class FormCreationMenu extends Component {
     }
 }
 
-// export class ChooseMenu extends Component {
-//     constructor(props) {
-//         super(props);//only leaving this here in case this.props is needed in the constructor
-//     }
+export class ChooseMenu extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
 
-//     render(){
+    render(){
+        return(
+            <div className="content">
+                {' '}
+                Choose New Goal Component Type From List
+            </div>
+        );
+    }
+}
 
-//     }
-// }
+export class YesNoGoal extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
 
-// export class YesNoGoal extends Component {
-//     constructor(props) {
-//         super(props);//only leaving this here in case this.props is needed in the constructor
-//     }
+    render(){
+         return(
+             <div className="content">
+                {' '}
+                This is a placeholder for yes/no goal creation
+             </div>
+       );
+    }
+}
 
-//     render(){
+export class TimerGoal extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
 
-//     }
-// }
+    render(){
+        return(
+            <div className="content">
+               {' '}
+               This is a placeholder for timer goal creation
+            </div>
+        );
+    }
+}
 
-// export class IncrementGoal extends Component {
-//     constructor(props) {
-//         super(props);//only leaving this here in case this.props is needed in the constructor
-//     }
+export class IncrementGoal extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
 
-//     render(){
-
-//     }
-// }
-
-
-
-// export class DropdownGoal extends Component {
-//     constructor(props) {
-//         super(props);//only leaving this here in case this.props is needed in the constructor
-//     }
-
-//     render(){
-
-//     }
-// }
+    render(){
+        return(
+            <div className="content">
+               {' '}
+               This is a placeholder for icrement goal creation
+            </div>
+        );
+    }
+}
 
 
+export class DropdownGoal extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
 
-// export class TextboxGoal extends Component {
-//     constructor(props) {
-//         super(props);//only leaving this here in case this.props is needed in the constructor
-//     }
+    render(){
+        return(
+            <div className="content">
+               {' '}
+               This is a placeholder for dropdown goal creation
+            </div>
+        );
+    }
+}
 
-//     render(){
 
-//     }
-// }
+
+export class TextboxGoal extends Component {
+    constructor(props) {
+        super(props);//only leaving this here in case this.props is needed in the constructor
+    }
+
+    render(){
+        return(
+            <div className="content">
+               {' '}
+               This is a placeholder for textbox goal creation
+            </div>
+        );
+    }
+}
